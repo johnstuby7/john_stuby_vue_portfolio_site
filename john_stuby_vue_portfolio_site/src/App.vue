5eeb620d8d80f0e13d7b15378d16e207d5426fc6
John Stuby <johnstuby7@gmail.com>
2023-10-16 09:20:12 -0600

    Added in footer component with basic information, added it to the project so it displays correctly at the bottom of the page, need to add css to ensure that it correctly stays in the center of the bottom of the page regardless of how a page is resized

--- john_stuby_vue_portfolio_site/src/App.vue
+++ john_stuby_vue_portfolio_site/src/App.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import FooterComponent from "../src/components/Footer_component.vue"
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+  <div id="app">
+    <div id="nav">
       <nav>
         <RouterLink to="/">Home</RouterLink> &nbsp;
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
+  <FooterComponent />
 </template>
---
+++ john_stuby_vue_portfolio_site/src/components/Footer_component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p class="footer">@ 2023 - John R Stuby</p>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.footer {
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+  height: 30px;
+}
+</style>
